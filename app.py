3ee0f57b957b034355bb60588bf3ebd3365e12e1
Samiur Rahman <99593559+mdsrer@users.noreply.github.com>
2024-02-26 23:10:51 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,55 @@
-from flask import Flask, request, render_template, send_file
-import pandas as pd
 import os
+
+import pandas as pd
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index_d.html')
+
+def read_file(file):
+    _, file_extension = os.path.splitext(file.filename)
+    if file_extension == '.xlsx':
+        return pd.read_excel(file)
+    elif file_extension == '.csv':
+        return pd.read_csv(file)
+    elif file_extension == '.txt':
+        return pd.read_csv(file, delimiter='\t')
+    else:
+        return None
+
+def save_file(df, filename):
+    _, file_extension = os.path.splitext(filename)
+    if file_extension == '.xlsx':
+        df.to_excel(filename, index=False)
+    elif file_extension == '.csv':
+        df.to_csv(filename, index=False)
+    elif file_extension == '.txt':
+        df.to_csv(filename, index=False, sep='\t')
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # if 'file1' not in request.files or 'file2' not in request.files:
-    #     return "Please select two files to upload."
-
     file1 = request.files['file1']
     file2 = request.files['file2']
 
     if file1.filename == '':
-        return render_template('index.html', alert_message="Please select at least 1 file to upload.")
-    
-    df1 = pd.read_excel(file1)
+        return render_template('index_d.html', alert_message="Please select at least 1 file to upload.")
+
+    df1 = read_file(file1)
     df1 = df1.drop_duplicates(subset='email')
 
     if file2.filename != '':
-        df2 = pd.read_excel(file2)
+        df2 = read_file(file2)
         # Remove rows in df1 where the email exists in df2
         df1 = df1[~df1['email'].isin(df2['email'])]
 
-    # Save the modified DataFrame to a new Excel file
-    output_file = 'output.xlsx'
-    df1.to_excel(output_file, index=False)
+    output_filename = 'output' + os.path.splitext(file1.filename)[1]
+    save_file(df1, output_filename)
 
-    # Return the modified Excel file for download
-    return send_file(output_file, as_attachment=True)
+    # Return the modified file for download
+    return send_file(output_filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
